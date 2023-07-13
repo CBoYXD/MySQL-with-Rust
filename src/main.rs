6d136faac0d99e2198138c1092cc197500103e1b
CBoYXD <135316445+CBoYXD@users.noreply.github.com>
2023-07-13 19:44:40 +0300

    Update main.rs
    Signed-off-by: CBoYXD <135316445+CBoYXD@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod examples;
 fn main(){
     // examples::create::create_table();
     // examples::insert::insert_data();
-    // examples::delete::delete_data(132132);
-    // examples::update::update_user(132132, "Danya");
+    // examples::delete::delete_data();
+    // examples::update::update_user();
     // examples::select::select_data();
 }
